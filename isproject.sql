da68ed5c8ad11879c2962d3b4978045bde3400d9
ronbrian <ronmachoka@gmail.com>
2018-10-31 00:45:56 +0300

    ..,

--- isproject.sql
+++ isproject.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 22, 2018 at 12:32 PM
+-- Generation Time: Oct 30, 2018 at 10:45 PM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 7.2.5
 
@@ -46,12 +46,58 @@ CREATE TABLE `appointments` (
 
 INSERT INTO `appointments` (`no`, `user id`, `name`, `current time`, `appointment date`, `appointment time`, `service`, `status`, `viewed`) VALUES
 (20, 36941325, 'Ron Brian', '2018/10/20/07:20', '2018-10-30', '14:00', 'Laboratory Tests', '', 1),
-(23, 36941325, 'Ron Brian', '2018/10/20/08:37', '', '', 'Doctor Consultation', '', 0),
-(24, 36941325, 'Ron Brian', '2018/10/20/08:40', '', '', 'Doctor Consultation', '', 0),
-(25, 36941325, 'Ron Brian', '2018/10/20/08:40', '', '', 'Doctor Consultation', '', 0),
-(26, 36941325, 'Ron Brian', '2018/10/20/08:41', '', '', 'Doctor Consultation', '', 0),
-(27, 36941325, 'Ron Brian', '2018/10/20/10:12', '2018-10-15', '12:07', 'Doctor Consultation', '', 0),
-(28, 1738, 'Mary Wambui', '2018/10/20/10:23', '2018-10-11', '15:58', 'Doctor Consultation', '', 0);
+(23, 36941325, 'Ron Brian', '2018/10/20/08:37', '', '', 'Doctor Consultation', '', 1),
+(24, 36941325, 'Ron Brian', '2018/10/20/08:40', '', '', 'Doctor Consultation', '', 1),
+(25, 36941325, 'Ron Brian', '2018/10/20/08:40', '', '', 'Doctor Consultation', '', 1),
+(26, 36941325, 'Ron Brian', '2018/10/20/08:41', '', '', 'Doctor Consultation', '', 1),
+(27, 36941325, 'Ron Brian', '2018/10/20/10:12', '2018-10-15', '12:07', 'Doctor Consultation', '', 1),
+(28, 1738, 'Mary Wambui', '2018/10/20/10:23', '2018-10-11', '15:58', 'Doctor Consultation', '', 1),
+(29, 36941325, 'Ron Brian', '2018/10/25/06:15', '2018-10-19', '04:01', 'Laboratory Tests', '', 1),
+(30, 0, '', '', '', '', '', '', 1),
+(31, 36941325, 'Ron Brian', '2018/10/25/06:19', '2018-10-17', '02:58', 'Physiotherapy', '', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inpatient`
+--
+
+CREATE TABLE `inpatient` (
+  `no` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `diagnosis` text NOT NULL,
+  `prescription` text NOT NULL,
+  `admission date` varchar(100) NOT NULL,
+  `discharge date` varchar(100) NOT NULL,
+  `duration` varchar(100) NOT NULL,
+  `ward no` varchar(100) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '0',
+  `nurse` text NOT NULL,
+  `doctor` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `inpatient`
+--
+
+INSERT INTO `inpatient` (`no`, `id`, `name`, `diagnosis`, `prescription`, `admission date`, `discharge date`, `duration`, `ward no`, `status`, `nurse`, `doctor`) VALUES
+(6, 36941325, 'Ron Brian', 'Migraines', 'Panadol', '', '', '', '', 1, '', 'mjuma'),
+(7, 12345678, 'James Mwangi', 'mo', 'om', '', '', '', '', 0, '', 'mjuma'),
+(8, 12345678, 'James Mwangi', '', '', '', '', '', '', 2, '', 'mjuma'),
+(9, 12345678, 'James Mwangi', '', '', '', '', '', '', 0, '', 'mjuma'),
+(10, 3, 'Test User 3', '', 'ffbfbf', '', '', '', '', 0, '', 'mjuma'),
+(11, 36941325, 'Ron Brian', 'finale', 'finale', '', '', '', '', 0, '', 'mjuma'),
+(12, 12345678, 'James Mwangi', '        document.getElementById(\"medicalinfox\").submit();', '        document.getElementById(\"medicalinfox\").submit();', '', '', '', '', 0, '', 'mjuma'),
+(13, 36941325, '//', '//', '//', '', '', '', '', 0, '', '//'),
+(14, 0, '//', '//', '//', '', '', '', '', 0, '', '//'),
+(15, 0, '//', '//', '//', '', '', '', '', 0, '', '//'),
+(16, 3, 'Test User 3', 'notin', 'notin', '', '', '', '', 0, '', 'mjuma'),
+(17, 36941325, 'Ron Brian', 'cvb', 'cvb', '', '', '', '', 0, '', 'mjuma'),
+(18, 12345678, 'James Mwangi', 'test', 'test', '', '', '', '', 0, '', 'mjuma'),
+(19, 36941325, 'Ron Brian', 'pre', 'pre', '', '', '', '', 0, '', 'mjuma'),
+(20, 12345678, 'James Mwangi', '', '', '', '', '', '', 0, '', 'mjuma'),
+(21, 36941325, 'fffffff', 'Migraines', '', 'gfg', 'fgfg', 'fgfg', 'fgff', 0, 'bnjogu', '');
 
 -- --------------------------------------------------------
 
@@ -165,7 +211,94 @@ INSERT INTO `logs` (`no`, `id`, `time`, `doctor`) VALUES
 (316, 36941325, '2018/10/20/07:20', 'mjuma'),
 (317, 12345678, '2018/10/20/08:34', 'mjuma'),
 (318, 3, '2018/10/20/08:55', 'mjuma'),
-(319, 36941325, '2018/10/20/10:37', 'mjuma');
+(319, 36941325, '2018/10/20/10:37', 'mjuma'),
+(320, 36941325, '2018/10/25/06:26', ''),
+(321, 36941325, '2018/10/25/07:24', ''),
+(322, 3, '2018/10/25/07:25', ''),
+(323, 3, '2018/10/25/07:26', ''),
+(324, 0, '2018/10/25/07:30', ''),
+(325, 0, '2018/10/25/07:35', ''),
+(326, 36941325, '2018/10/25/07:36', ''),
+(327, 36941325, '2018/10/26/08:14', ''),
+(328, 44, '2018/10/26/08:15', ''),
+(329, 36941325, '2018/10/27/01:00', ''),
+(330, 36941325, '2018/10/27/01:08', ''),
+(331, 36941325, '2018/10/27/01:10', ''),
+(332, 36941325, '2018/10/27/01:26', 'mjuma'),
+(333, 36941325, '2018/10/27/10:15', 'mjuma'),
+(334, 12345678, '2018/10/27/10:18', 'mjuma'),
+(335, 90, '2018/10/27/10:19', 'mjuma'),
+(336, 12345678, '2018/10/27/10:19', 'mjuma'),
+(337, 36941325, '2018/10/27/10:21', 'mjuma'),
+(338, 3, '2018/10/27/10:24', 'mjuma'),
+(339, 12345678, '2018/10/27/10:24', 'mjuma'),
+(340, 67654343, '2018/10/27/10:25', 'mjuma'),
+(341, 12345678, '2018/10/27/10:25', 'mjuma'),
+(342, 12345678, '2018/10/27/10:27', 'mjuma'),
+(343, 12345678, '2018/10/27/10:28', 'mjuma'),
+(344, 36941325, '2018/10/27/10:29', 'mjuma'),
+(345, 3, '2018/10/27/10:30', 'mjuma'),
+(346, 36941325, '2018/10/27/10:31', 'mjuma'),
+(347, 36941325, '2018/10/27/10:37', 'mjuma'),
+(348, 36941325, '2018/10/27/10:38', 'mjuma'),
+(349, 3, '2018/10/27/10:39', 'mjuma'),
+(350, 36941325, '2018/10/27/10:40', 'mjuma'),
+(351, 12345678, '2018/10/27/10:42', 'mjuma'),
+(352, 36941325, '2018/10/27/10:43', 'mjuma'),
+(353, 36941325, '2018/10/27/10:44', 'mjuma'),
+(354, 36941325, '2018/10/27/10:46', 'mjuma'),
+(355, 676543, '2018/10/27/10:47', 'mjuma'),
+(356, 36941325, '2018/10/27/10:50', 'mjuma'),
+(357, 12345678, '2018/10/27/10:53', 'mjuma'),
+(358, 36941325, '2018/10/27/10:54', 'mjuma'),
+(359, 12345678, '2018/10/27/10:54', 'mjuma'),
+(360, 36941325, '2018/10/27/11:23', 'mjuma'),
+(361, 36941325, '2018/10/27/11:24', 'mjuma'),
+(362, 12345678, '2018/10/27/11:26', 'mjuma'),
+(363, 12345678, '2018/10/27/11:28', 'mjuma'),
+(364, 3, '2018/10/27/11:28', 'mjuma'),
+(365, 676543, '2018/10/27/11:28', 'mjuma'),
+(366, 36941325, '2018/10/27/11:31', 'mjuma'),
+(367, 0, '2018/10/27/11:33', 'mjuma'),
+(368, 12345678, '2018/10/27/11:37', 'mjuma'),
+(369, 12345678, '2018/10/27/11:40', 'mjuma'),
+(370, 3, '2018/10/27/11:41', 'mjuma'),
+(371, 12345678, '2018/10/27/11:42', 'mjuma'),
+(372, 36941325, '2018/10/27/11:42', 'mjuma'),
+(373, 3, '2018/10/27/11:43', 'mjuma'),
+(374, 3, '2018/10/27/11:44', 'mjuma'),
+(375, 3, '2018/10/27/11:46', 'mjuma'),
+(376, 36941325, '2018/10/27/11:47', 'mjuma'),
+(377, 3, '2018/10/27/11:47', 'mjuma'),
+(378, 3, '2018/10/27/11:48', 'mjuma'),
+(379, 36941325, '2018/10/27/11:54', 'mjuma'),
+(380, 36941325, '2018/10/27/11:58', 'mjuma'),
+(381, 36941325, '2018/10/27/11:59', 'mjuma'),
+(382, 12345678, '2018/10/28/12:00', 'mjuma'),
+(383, 36941325, '2018/10/28/12:03', 'mjuma'),
+(384, 12345678, '2018/10/28/12:04', 'mjuma'),
+(385, 36941325, '2018/10/28/05:07', '123'),
+(386, 3, '2018/10/28/05:08', '123'),
+(387, 36941325, '2018/10/29/10:06', 'mjuma'),
+(388, 3, '2018/10/29/10:08', 'mjuma'),
+(389, 3, '2018/10/29/10:09', 'mjuma'),
+(390, 36941325, '2018/10/29/10:12', 'mjuma'),
+(391, 3, '2018/10/29/10:21', 'mjuma'),
+(392, 36941325, '2018/10/29/10:23', 'mjuma'),
+(393, 36941325, '2018/10/29/10:24', 'mjuma'),
+(394, 3, '2018/10/29/10:45', 'mjuma'),
+(395, 36941325, '2018/10/29/10:47', 'mjuma'),
+(396, 36941325, '2018/10/29/10:49', 'mjuma'),
+(397, 36941325, '2018/10/29/10:54', 'mjuma'),
+(398, 3, '2018/10/29/10:55', 'mjuma'),
+(399, 12345678, '2018/10/29/10:55', 'mjuma'),
+(400, 36941325, '2018/10/29/10:56', 'mjuma'),
+(401, 12345678, '2018/10/29/10:58', 'mjuma'),
+(402, 12345678, '2018/10/29/10:58', 'mjuma'),
+(403, 36941325, '2018/10/29/10:59', 'mjuma'),
+(404, 12345678, '2018/10/29/11:04', 'mjuma'),
+(405, 36941325, '2018/10/30/10:56', 'mjuma'),
+(406, 36941325, '2018/10/31/12:13', 'mjuma');
 
 -- --------------------------------------------------------
 
@@ -198,7 +331,25 @@ INSERT INTO `medical info` (`no`, `id`, `symptoms`, `period`, `diagnosis`, `pres
 (28, 12345678, '', '', '', '', '', '', '2018/10/07/09:14', ''),
 (29, 36941325, 'Stomach Ache', '4 Months', 'Ulcers / Stomach Ulcers', '', 'Take with Plenty of water, and do not skip meals.', 'mjuma', '2018/10/14/05:36', ''),
 (30, 36941325, 'Headache', '3 days', 'Lack of sleep', '', 'Take with a glass of plain water', 'mjuma', '2018/10/14/05:39', ''),
-(31, 36941325, 'Unknown', 'Not Sure', 'Nothing', '', 'No Etra Notes', 'jmwangi', '2018/10/14/05:56', 'Kenyatta National Hospital');
+(31, 36941325, 'Unknown', 'Not Sure', 'Nothing', '', 'No Etra Notes', 'jmwangi', '2018/10/14/05:56', 'Kenyatta National Hospital'),
+(32, 36941325, '', '', '', '', '', '', '2018/10/25/07:23', 'Kenyatta National Hospital'),
+(33, 36941325, '', '', '', '', '', '', '2018/10/25/07:24', 'Kenyatta National Hospital'),
+(34, 36941325, 'Headache', '2 weeks', 'Migraines', '', '', 'mjuma', '2018/10/27/11:32', 'Kenyatta National Hospital'),
+(35, 12345678, '', '', 'mo', '', '', 'mjuma', '2018/10/27/11:37', 'Kenyatta National Hospital'),
+(36, 12345678, '', '', '', '', '', 'mjuma', '2018/10/27/11:39', 'Kenyatta National Hospital'),
+(37, 12345678, '', '', '', '', '', 'mjuma', '2018/10/27/11:40', 'Kenyatta National Hospital'),
+(38, 36941325, 'nothing', 'nothing', 'nothing', '', 'nothing', 'mjuma', '2018/10/27/11:42', 'Kenyatta National Hospital'),
+(39, 3, '', '', '', '', '', 'mjuma', '2018/10/27/11:44', 'Kenyatta National Hospital'),
+(40, 3, '', '', '', '', '', 'mjuma', '2018/10/27/11:44', 'Kenyatta National Hospital'),
+(41, 3, '', '', '', '', '', 'mjuma', '2018/10/27/11:48', 'Kenyatta National Hospital'),
+(42, 36941325, 'id=\"medicalinfo\"', 'id=\"medicalinfo\"', 'id=\"medicalinfo\"', '', 'id=\"medicalinfo\"', 'mjuma', '2018/10/27/11:58', 'Kenyatta National Hospital'),
+(43, 3, 'notin', 'notin', 'notin', 'notin', 'vnotin', 'mjuma', '2018/10/29/10:46', 'Kenyatta National Hospital'),
+(44, 36941325, 'cvb', 'cvb', 'cvb', 'cvb', 'cvb', 'mjuma', '2018/10/29/10:55', 'Kenyatta National Hospital'),
+(45, 12345678, 'test', 'test', 'test', 'test', 'test', 'mjuma', '2018/10/29/10:56', 'Kenyatta National Hospital'),
+(46, 36941325, 'old', 'old', 'old', '', 'old', 'mjuma', '2018/10/29/10:56', 'Kenyatta National Hospital'),
+(47, 12345678, 'pre', 'pre', 'pre', 'pre', 'pre', 'mjuma', '2018/10/29/10:58', 'Kenyatta National Hospital'),
+(48, 36941325, 'pre', 'pre', 'pre', 'pre', 'pre', 'mjuma', '2018/10/29/10:59', 'Kenyatta National Hospital'),
+(49, 12345678, '', '', '', '', '', 'mjuma', '2018/10/29/11:04', 'Kenyatta National Hospital');
 
 -- --------------------------------------------------------
 
@@ -281,7 +432,7 @@ INSERT INTO `staff` (`Name`, `username`, `ID`, `title`, `password`) VALUES
 ('James Mwangi', 'jmwangi', 1, 'doctor', '123'),
 ('Hellen Njoki', 'hnjoki', 2, 'clerk', 'qwe'),
 ('mzee juma', 'mjuma', 3, 'doctor', 'zxc'),
-('Brian Njogu', 'bnjogu', 4, 'clerk', 'asd');
+('Brian Njogu', 'bnjogu', 4, 'nurse', 'asd');
 
 -- --------------------------------------------------------
 
@@ -328,6 +479,12 @@ ALTER TABLE `appointments`
   ADD UNIQUE KEY `no` (`no`),
   ADD KEY `user id` (`user id`),
   ADD KEY `no_2` (`no`);
+
+--
+-- Indexes for table `inpatient`
+--
+ALTER TABLE `inpatient`
+  ADD PRIMARY KEY (`no`);
 
 --
 -- Indexes for table `login`
@@ -385,19 +542,25 @@ ALTER TABLE `staff`
 -- AUTO_INCREMENT for table `appointments`
 --
 ALTER TABLE `appointments`
-  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- AUTO_INCREMENT for table `inpatient`
+--
+ALTER TABLE `inpatient`
+  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `logs`
 --
 ALTER TABLE `logs`
-  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=320;
+  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=407;
 
 --
 -- AUTO_INCREMENT for table `medical info`
 --
 ALTER TABLE `medical info`
-  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT for table `patient`
